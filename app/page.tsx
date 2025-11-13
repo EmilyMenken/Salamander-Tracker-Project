f4687bf393ca27a2da938b890ecc53af42226b5f
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-11-13 14:44:42 -0800

    made a fetch call on combined api

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,26 @@
 import Image from "next/image";
 
-export default function Home() {
+// app/dashboard/page.tsx
+export default async function DashboardPage() {
+  {/* http://localhost:3000/api/process: */}
+
+    {/* targetColor = hex number, threshold = 100 */}
+    {/* http://localhost:3000/api/process/:fileName?targetColor=654321&threshold=100 */}
+
+  {/* http://localhost:3000/api/thumbnail/:fileName */}
+  {/* http://localhost:3000/api/videos */}
+  {/* http://localhost:3000/api/process:jobIDnumber/status */}
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/combined`, {
+    cache: "no-store",
+  });
+  const data = await res.json();
+
   return (
-      <div>
-      
-      {/* http://localhost:3000/api/process: */}
-
-        {/* targetColor = hex number, threshold = 100 */}
-        {/* http://localhost:3000/api/process/:fileName?targetColor=654321&threshold=100 */}
-
-      {/* http://localhost:3000/api/thumbnail/:fileName */}
-      {/* http://localhost:3000/api/videos */}
-      {/* http://localhost:3000/api/process:jobIDnumber/status */}
-      </div>
+    <main>
+      <h1>Dashboard</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </main>
   );
 }
+
