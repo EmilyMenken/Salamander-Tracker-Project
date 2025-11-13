cf6b389f9165f59debb0247cac952b703ed71dfe
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-11-13 14:39:31 -0800

    changed the endpoints a lil and added routes.ts

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
       <div>
       
-      {/* http://localhost:3000/api/process/ */}
+      {/* http://localhost:3000/api/process: */}
 
         {/* targetColor = hex number, threshold = 100 */}
-        {/* http://localhost:3000/api/process/ballsMoving.mp4?targetColor=654321&threshold=100 */}
+        {/* http://localhost:3000/api/process/:fileName?targetColor=654321&threshold=100 */}
 
-      {/* http://localhost:3000/api/thumbnail/ */}
+      {/* http://localhost:3000/api/thumbnail/:fileName */}
       {/* http://localhost:3000/api/videos */}
-      {/* http://localhost:3000/api/process/jobIDnumber/status */}
+      {/* http://localhost:3000/api/process:jobIDnumber/status */}
       </div>
   );
 }
