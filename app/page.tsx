1a2bbf184946b00925e5dabd49a4cac621316aa4
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-11-20 13:02:26 -0800

    commiting before pulling

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default async function DashboardPage() {
   {/* http://localhost:3000/api/process: */}
 
-    {/* targetColor = hex number, threshold = 100 */}
-    {/* http://localhost:3000/api/process/:fileName?targetColor=654321&threshold=100 */}
+  {/* targetColor = hex number, threshold = 100 */}
+  {/* http://localhost:3000/api/process/:fileName?targetColor=654321&threshold=100 */}
 
   {/* http://localhost:3000/api/thumbnail/:fileName */}
   {/* http://localhost:3000/api/videos */}
